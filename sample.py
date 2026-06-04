90a4fbce325e1ab5b0c9681cd2c3446272c243e1
blkdmr <alessio.russo.ar98@gmail.com>
2026-06-05 00:34:10 +0200

    slides update

--- sample.py
+++ sample.py
@@ -73,7 +73,7 @@ def solve_instance(box_x, box_y, pallet_x, pallet_y, solver_name="gecode"):
 
 def main():
     box_x, box_y = 70, 70
-    pallet_x, pallet_y = 800, 1200
+    pallet_x, pallet_y = 800, 400
 
     solution = solve_instance(box_x, box_y, pallet_x, pallet_y, solver_name="gecode")
 
